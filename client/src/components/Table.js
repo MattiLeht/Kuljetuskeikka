9f6ff21d1e228eaf4cfd4c332d8e119808cbf146
jopinihu <111080671+jopinihu@users.noreply.github.com>
2022-11-03 10:48:00 +0200

    make it better

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,6 +1,5 @@
-import React, { useState, Fragment, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../style.css";
-
 import axios from "axios";
 // import { useState } from 'react'
 // import "./Table.css"
@@ -15,6 +14,7 @@ const Table = () => {
   const [mass, setMass] = useState("");
 
   const [loadList, setLoadList] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:3008/api/get/").then((response) => {
       setLoadList(response.data);
@@ -42,30 +42,8 @@ const Table = () => {
     loadList(event.target.value);
   };
 
-  const updateLoads = (id) => {
-    axios
-      // Säädetty
-      .put("http://localhost:3008/api/update/", {
-        sender: sender,
-        recipient: recipient,
-        product: product,
-        vehicle: vehicle,
-        number: number,
-        mass: mass,
-        id: id,
-      })
-      .then((response) => {
-        alert("update");
-      });
-  };
-
-  // const handleEditClick = (event, val) => {
-  //   event.preventDeafult();
-  //   SetUpdateId(val.id);
-  // };
-
-  const deleteLoad = (kuorma) => {
-    axios.delete(`http://localhost:3008/api/delete/${kuorma}`);
+  const deleteLoad = (id) => {
+    axios.delete(`http://localhost:3008/api/delete/${id}`);
   };
 
   return (
@@ -114,70 +92,65 @@ const Table = () => {
               );
             })}
           </tbody>
-          <tfoot>
-            <div
-              className="adding_load"
-              class="table-light container-fluid px-0"
-            >
-              <form className="inputs">
-                <input
-                  className="field1"
-                  type="text"
-                  name="sender"
-                  required="required"
-                  placeholder="Lahettaja"
-                  onChange={(e) => {
-                    setSender(e.target.value);
-                  }}
-                />
-                <input
-                  className="field2"
-                  type="text"
-                  name="recipient"
-                  required="required"
-                  placeholder="Vastaanottaja"
-                  onChange={(e) => {
-                    setSender(e.target.value);
-                  }}
-                />
-                <input
-                  type="text"
-                  name="product"
-                  placeholder="Tuote"
-                  onChange={(e) => {
-                    setProduct(e.target.value);
-                  }}
-                />
-                <input
-                  type="text"
-                  name="vehicle"
-                  placeholder="Auto"
-                  onChange={(e) => {
-                    setVehicle(e.target.value);
-                  }}
-                />
-                <input
-                  type="text"
-                  name="number"
-                  placeholder="Numero"
-                  onChange={(e) => {
-                    setNumber(e.target.value);
-                  }}
-                />
-                <input
-                  type="text"
-                  name="mass"
-                  placeholder="kg/m3"
-                  onChange={(e) => {
-                    setMass(e.target.value);
-                  }}
-                />
-                <button onClick={submitLoads}>Lisää</button>
-              </form>
-            </div>
-          </tfoot>
         </table>
       </form>
+      <div className="adding_load" class="table-light container-fluid px-0">
+        <form className="inputs">
+          <input
+            className="field1"
+            type="text"
+            name="sender"
+            required="required"
+            placeholder="Lahettaja"
+            onChange={(e) => {
+              setSender(e.target.value);
+            }}
+          />
+          <input
+            className="field2"
+            type="text"
+            name="recipient"
+            required="required"
+            placeholder="Vastaanottaja"
+            onChange={(e) => {
+              setRecipient(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            name="product"
+            placeholder="Tuote"
+            onChange={(e) => {
+              setProduct(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            name="vehicle"
+            placeholder="Auto"
+            onChange={(e) => {
+              setVehicle(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            name="number"
+            placeholder="Numero"
+            onChange={(e) => {
+              setNumber(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            name="mass"
+            placeholder="kg/m3"
+            onChange={(e) => {
+              setMass(e.target.value);
+            }}
+          />
+          <button onClick={submitLoads}>Lisää</button>
+        </form>
+      </div>
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,73 +1,86 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors')
-const mysql = require('mysql')
+const cors = require("cors");
+const mysql = require("mysql");
+require("dotenv").config();
+
 // Set database paramets
 const db = mysql.createPool({
-    host:'127.0.0.1',
-    port: "3307",
-    user: "root",
-    password:"password",
-    database: "keskimaki",
+  host: "127.0.0.1",
+  port: "3307",
+  user: "root",
+  password: "password",
+  database: "keskimaki",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Get data in database
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM loads";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
   });
+});
+// Add data to database
+app.post("/api/insert/", (req, res) => {
+  const sender = req.body.sender;
+  const recipient = req.body.recipient;
+  const product = req.body.product;
+  const vehicle = req.body.vehicle;
+  const number = req.body.number;
+  const mass = req.body.mass;
 
-    app.use(cors());
-    app.use(express.json())
-    app.use(bodyParser.urlencoded({extended: true}));
-    // Get data in database
-    app.get("/api/get", (req,res) => {
-      const sqlSelect = "SELECT * FROM loads";
-      db.query(sqlSelect, (err, result)=> {
-        res.send(result);
-      });
-    });
-    // Add data to database
-    app.post("/api/insert/", (req,res)=> {
-    const sender = req.body.sender;
-    const recipient = req.body.recipient;
-    const product = req.body.product;
-    const vehicle = req.body.vehicle;
-    const number = req.body.number;
-    const mass = req.body.mass;
-    
-    const sqlInsert = "INSERT INTO loads (sender, recipient, product, vehicle, number, mass) VALUES (?,?,?,?,?,?)"
-    db.query(sqlInsert, [sender, recipient, product, vehicle, number, mass], (err, result)=> {
-        console.log(result);
-      });
-    });
+  const sqlInsert =
+    "INSERT INTO loads (sender, recipient, product, vehicle, number, mass) VALUES (?,?,?,?,?,?)";
+  db.query(
+    sqlInsert,
+    [sender, recipient, product, vehicle, number, mass],
+    (err, result) => {
+      console.log(result);
+    }
+  );
+});
 
-  //   app.put("/api/update", (req, res) =>{
-  //     const sender = req.body.sender;
-  //   const recipient = req.body.recipient;
-  //   const product = req.body.product;
-  //   const vehicle = req.body.vehicle;
-  //   const number = req.body.number;
-  //   const mass = req.body.mass;
-  //   const sqlUpdate = "UPDATE SET loads sender = ? WHERE sender = ?";
-    
-  //   db.query(sqlUpdate, [mass,number,vehicle,product,recipient,sender], (err,result) => {
-  //     if (err) console.log(err);
-  //    });
-  //  });
+app.put("/api/update", (req, res) => {
+  const id = req.body.id;
+  const sender = req.body.sender;
+  const recipient = req.body.recipient;
+  const product = req.body.product;
+  const vehicle = req.body.vehicle;
+  const number = req.body.number;
+  const mass = req.body.mass;
 
+  const sqlUpdate =
+    "UPDATE loads SET sender = ?, recipient = ?, product = ?, vehicle = ?, number = ?, mass = ? WHERE id = ? ";
+  if (req)
+    db.query(
+      sqlUpdate,
+      [sender, recipient, product, vehicle, number, mass, id],
+      (err, result) => {
+        if (err) console.log(err);
+      }
+    );
+});
 
 // Delete row in table
-app.delete("/api/delete/:sender", (req, res) => {
-  const sender = req.params.sender;
-
-  const sqlDelete = "DELETE FROM loads WHERE sender = ?";
-  db.query(sqlDelete, sender, (err, result) => {
-    if (err) console.log(err);
+// Delete row in table
+app.delete("/api/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM loads WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send(result);
+    }
   });
 });
 
-    app.listen(3008, () => {
-        console.log("running on port 3008");
-      })
+app.listen(process.env.PORT || 3008, () => {
+  console.log("running on port 3008");
+});
 
-      // npm init
-    //  npm run dev käynnistää console.login
-    // npm install cors body-parser express nodemon mysql
-
-  
+// npm init
+//  npm run dev käynnistää console.login
+// npm install cors body-parser express nodemon mysql
